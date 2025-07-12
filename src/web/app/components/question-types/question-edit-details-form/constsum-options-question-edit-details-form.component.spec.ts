8bb9f7ee89002d8cc1cc7f8d94f3d7d5794177a0
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2025-07-11 23:44:23 -0300

    Refactor HTML and TypeScript for ConstsumOptionsQuestionEditDetailsFormComponent; improve formatting and add radio group isolation functionality

--- src/web/app/components/question-types/question-edit-details-form/constsum-options-question-edit-details-form.component.spec.ts
+++ src/web/app/components/question-types/question-edit-details-form/constsum-options-question-edit-details-form.component.spec.ts
@@ -1,85 +1,160 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { ConstsumOptionsFieldComponent } from './constsum-options-field/constsum-options-field.component';
-import {
-  ConstsumOptionsQuestionEditDetailsFormComponent,
-} from './constsum-options-question-edit-details-form.component';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { ConstsumOptionsFieldComponent } from "./constsum-options-field/constsum-options-field.component";
+import { ConstsumOptionsQuestionEditDetailsFormComponent } from "./constsum-options-question-edit-details-form.component";
 
-describe('ConstsumOptionsQuestionEditDetailsFormComponent', () => {
+describe("ConstsumOptionsQuestionEditDetailsFormComponent", () => {
   let component: ConstsumOptionsQuestionEditDetailsFormComponent;
   let fixture: ComponentFixture<ConstsumOptionsQuestionEditDetailsFormComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        FormsModule,
-        DragDropModule,
-      ],
+      imports: [FormsModule, DragDropModule],
       declarations: [
         ConstsumOptionsQuestionEditDetailsFormComponent,
         ConstsumOptionsFieldComponent,
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConstsumOptionsQuestionEditDetailsFormComponent);
+    fixture = TestBed.createComponent(
+      ConstsumOptionsQuestionEditDetailsFormComponent
+    );
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should prevent alphabetical character inputs in onIntegerInput', () => {
-    const event = new KeyboardEvent('keypress', {
-      key: 'b',
+  it("should prevent alphabetical character inputs in onIntegerInput", () => {
+    const event = new KeyboardEvent("keypress", {
+      key: "b",
     });
 
-    const eventSpy = jest.spyOn(event, 'preventDefault');
+    const eventSpy = jest.spyOn(event, "preventDefault");
     component.onIntegerInput(event);
     expect(eventSpy).toHaveBeenCalled();
   });
 
-  it('should prevent decimal point inputs in onIntegerInput', () => {
-    const event = new KeyboardEvent('keypress', {
-      key: '.',
+  it("should prevent decimal point inputs in onIntegerInput", () => {
+    const event = new KeyboardEvent("keypress", {
+      key: ".",
     });
 
-    const eventSpy = jest.spyOn(event, 'preventDefault');
+    const eventSpy = jest.spyOn(event, "preventDefault");
     component.onIntegerInput(event);
     expect(eventSpy).toHaveBeenCalled();
   });
 
-  it('should allow digit inputs in onIntegerInput', () => {
-    const event = new KeyboardEvent('keypress', {
-      key: '7',
+  it("should allow digit inputs in onIntegerInput", () => {
+    const event = new KeyboardEvent("keypress", {
+      key: "7",
     });
 
-    const eventSpy = jest.spyOn(event, 'preventDefault');
+    const eventSpy = jest.spyOn(event, "preventDefault");
     component.onIntegerInput(event);
     expect(eventSpy).not.toHaveBeenCalled();
   });
 
-  it('should allow number input with less than or equal to 9 digits', () => {
-    const inputElement = fixture.debugElement.query(By.css('#max-point')).nativeElement as HTMLInputElement;
-    const inputEvent = new InputEvent('input');
+  it("should allow number input with less than or equal to 9 digits", () => {
+    const inputElement = fixture.debugElement.query(By.css("#max-point"))
+      .nativeElement as HTMLInputElement;
+    const inputEvent = new InputEvent("input");
     inputElement.dispatchEvent(inputEvent);
-    (inputEvent.target as HTMLInputElement).value = '12345';
-    component.restrictIntegerInputLength(inputEvent, 'points');
-    expect((inputEvent.target as HTMLInputElement).value).toEqual('12345');
+    (inputEvent.target as HTMLInputElement).value = "12345";
+    component.restrictIntegerInputLength(inputEvent, "points");
+    expect((inputEvent.target as HTMLInputElement).value).toEqual("12345");
   });
 
-  it('should restrict number input with more than 9 digits to 9 digits', () => {
-    const inputElement = fixture.debugElement.query(By.css('#max-point')).nativeElement as HTMLInputElement;
-    const inputEvent = new InputEvent('input');
+  it("should restrict number input with more than 9 digits to 9 digits", () => {
+    const inputElement = fixture.debugElement.query(By.css("#max-point"))
+      .nativeElement as HTMLInputElement;
+    const inputEvent = new InputEvent("input");
     inputElement.dispatchEvent(inputEvent);
-    (inputEvent.target as HTMLInputElement).value = '123456789012345';
-    component.restrictIntegerInputLength(inputEvent, 'points');
-    expect((inputEvent.target as HTMLInputElement).value).toEqual('123456789');
+    (inputEvent.target as HTMLInputElement).value = "123456789012345";
+    component.restrictIntegerInputLength(inputEvent, "points");
+    expect((inputEvent.target as HTMLInputElement).value).toEqual("123456789");
+  });
+
+  it("should render separate Constant Sum question instances with independent options", () => {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    const fixture1 = TestBed.createComponent(
+      ConstsumOptionsQuestionEditDetailsFormComponent
+    );
+    const fixture2 = TestBed.createComponent(
+      ConstsumOptionsQuestionEditDetailsFormComponent
+    );
+
+    fixture1.componentInstance.model.constSumOptions = ["Option 1", "Option 2"];
+    fixture2.componentInstance.model.constSumOptions = [
+      "Option A",
+      "Option B",
+      "Option C",
+    ];
+
+    container.appendChild(fixture1.nativeElement);
+    container.appendChild(fixture2.nativeElement);
+
+    fixture1.detectChanges();
+    fixture2.detectChanges();
+
+    const radios1 = fixture1.nativeElement.querySelectorAll(
+      '#radio-options-test-wrapper input[type="radio"]'
+    );
+    const radios2 = fixture2.nativeElement.querySelectorAll(
+      '#radio-options-test-wrapper input[type="radio"]'
+    );
+
+    expect(radios1.length).toBe(2);
+    expect(radios2.length).toBe(3);
+
+    (radios1[0] as HTMLInputElement).click();
+    fixture1.detectChanges();
+
+    expect((radios2[0] as HTMLInputElement).checked).toBe(false);
+
+    container.remove();
   });
 });
+
+it.only("should isolate radio selections between different instances", waitForAsync(() => {
+  const fixture1 = TestBed.createComponent(
+    ConstsumOptionsQuestionEditDetailsFormComponent
+  );
+  const fixture2 = TestBed.createComponent(
+    ConstsumOptionsQuestionEditDetailsFormComponent
+  );
+
+  fixture1.componentInstance.model.constSumOptions = ["Option A", "Option B"];
+  fixture2.componentInstance.model.constSumOptions = ["Option X", "Option Y"];
+
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+  container.appendChild(fixture1.nativeElement);
+  container.appendChild(fixture2.nativeElement);
+
+  fixture1.detectChanges();
+  fixture2.detectChanges();
+
+  // Use nativeElement.querySelectorAll to ensure DOM elements are queried
+  const radio1 = fixture1.nativeElement.querySelectorAll('input[type="radio"]');
+  const radio2 = fixture2.nativeElement.querySelectorAll('input[type="radio"]');
+
+  expect(radio1.length).toBeGreaterThan(0);
+  expect(radio2.length).toBeGreaterThan(0);
+
+  (radio1[0] as HTMLInputElement).click();
+  fixture1.detectChanges();
+
+  expect((radio1[0] as HTMLInputElement).checked).toBe(true);
+  expect((radio2[0] as HTMLInputElement).checked).toBe(false);
+
+  container.remove();
+}));
--- src/web/app/components/question-types/question-edit-details-form/constsum-options-question-edit-details-form.component.ts
+++ src/web/app/components/question-types/question-edit-details-form/constsum-options-question-edit-details-form.component.ts
@@ -1,27 +1,33 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
-import { QuestionEditDetailsFormComponent } from './question-edit-details-form.component';
-import { StatusMessageService } from '../../../../services/status-message.service';
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { Component } from "@angular/core";
+import { StatusMessageService } from "../../../../services/status-message.service";
 import {
   FeedbackConstantSumDistributePointsType,
   FeedbackConstantSumQuestionDetails,
-} from '../../../../types/api-output';
-import { DEFAULT_CONSTSUM_OPTIONS_QUESTION_DETAILS } from '../../../../types/default-question-structs';
+} from "../../../../types/api-output";
+import { DEFAULT_CONSTSUM_OPTIONS_QUESTION_DETAILS } from "../../../../types/default-question-structs";
+import { QuestionEditDetailsFormComponent } from "./question-edit-details-form.component";
 
 /**
  * Question details edit form component for constsum options question.
  */
 @Component({
-  selector: 'tm-constsum-options-question-edit-details-form',
-  templateUrl: './constsum-options-question-edit-details-form.component.html',
-  styleUrls: ['./constsum-options-question-edit-details-form.component.scss', './cdk-drag-drop.scss'],
+  selector: "tm-constsum-options-question-edit-details-form",
+  templateUrl: "./constsum-options-question-edit-details-form.component.html",
+  styleUrls: [
+    "./constsum-options-question-edit-details-form.component.scss",
+    "./cdk-drag-drop.scss",
+  ],
 })
-export class ConstsumOptionsQuestionEditDetailsFormComponent
-    extends QuestionEditDetailsFormComponent<FeedbackConstantSumQuestionDetails> {
+export class ConstsumOptionsQuestionEditDetailsFormComponent extends QuestionEditDetailsFormComponent<FeedbackConstantSumQuestionDetails> {
+  uniqueId = Math.random().toString(36).substring(2);
 
+  get radioGroupName(): string {
+    return `constsum-radio-${this.uniqueId}`;
+  }
   // enum
   FeedbackConstantSumDistributePointsType: typeof FeedbackConstantSumDistributePointsType =
-      FeedbackConstantSumDistributePointsType;
+    FeedbackConstantSumDistributePointsType;
 
   constructor(private statusMessageService: StatusMessageService) {
     super(DEFAULT_CONSTSUM_OPTIONS_QUESTION_DETAILS());
@@ -40,7 +46,7 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
    */
   increaseNumberOfConstsumOptions(): void {
     const newOptions: string[] = this.model.constSumOptions.slice();
-    newOptions.push('');
+    newOptions.push("");
 
     this.triggerModelChangeBatch({
       constSumOptions: newOptions,
@@ -58,7 +64,7 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
     const newOptions: string[] = this.model.constSumOptions.slice();
     moveItemInArray(newOptions, event.previousIndex, event.currentIndex);
 
-    this.triggerModelChange('constSumOptions', newOptions);
+    this.triggerModelChange("constSumOptions", newOptions);
   }
 
   /**
@@ -73,7 +79,9 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
    */
   onConstsumOptionDeleted(event: number): void {
     if (this.model.constSumOptions.length <= 2) {
-      this.statusMessageService.showErrorToast('There must be at least one option.');
+      this.statusMessageService.showErrorToast(
+        "There must be at least one option."
+      );
       return;
     }
 
@@ -92,7 +100,7 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
     const newOptions: string[] = this.model.constSumOptions.slice();
     newOptions[index] = event;
 
-    this.triggerModelChange('constSumOptions', newOptions);
+    this.triggerModelChange("constSumOptions", newOptions);
   }
 
   /**
@@ -101,8 +109,9 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
   onForceUnevenDistribution(event: boolean): void {
     this.triggerModelChangeBatch({
       forceUnevenDistribution: event,
-      distributePointsFor: event ? FeedbackConstantSumDistributePointsType.DISTRIBUTE_ALL_UNEVENLY
-          : FeedbackConstantSumDistributePointsType.NONE,
+      distributePointsFor: event
+        ? FeedbackConstantSumDistributePointsType.DISTRIBUTE_ALL_UNEVENLY
+        : FeedbackConstantSumDistributePointsType.NONE,
     });
   }
 
@@ -111,9 +120,9 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
    */
   resetMaxPoint(event: boolean): void {
     if (event) {
-      this.triggerModelChange('maxPoint', 0);
+      this.triggerModelChange("maxPoint", 0);
     } else {
-      this.triggerModelChange('maxPoint', undefined);
+      this.triggerModelChange("maxPoint", undefined);
     }
   }
 
@@ -122,9 +131,9 @@ export class ConstsumOptionsQuestionEditDetailsFormComponent
    */
   resetMinPoint(event: boolean): void {
     if (event) {
-      this.triggerModelChange('minPoint', 0);
+      this.triggerModelChange("minPoint", 0);
     } else {
-      this.triggerModelChange('minPoint', undefined);
+      this.triggerModelChange("minPoint", undefined);
     }
   }
 }
